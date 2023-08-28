3a7ff90b04c00e84132edef469cff821851d27e3
Alex Koniushenko <alex.koniushenko@gmail.com>
2023-08-28 11:17:13 +0300

    Code refactor: file handling updated

--- Modules/file_handling.py
+++ Modules/file_handling.py
@@ -8,16 +8,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+NUMBER_OF_RECORDS = 10
+
 
 # Open the website and download the Excel file
-def download_excel(driver, website, download_text, file_name, data_folder):
+def download_excel(driver, website, download_text, f_name):
     driver.get(website)
     download_link = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable(
             (By.XPATH, f'//a[contains(text(), "{download_text}")]')
         )
     )
-    f_name = f"./{data_folder}/{file_name}"
     if os.path.exists(f_name):
         os.remove(f_name)
     download_link.click()
@@ -35,17 +36,16 @@ def download_excel(driver, website, download_text, file_name, data_folder):
 
 
 # Load the Excel workbook and process data
-def process_excel_data(file_name, data_folder):
-    f_name = f"./{data_folder}/{file_name}"
+def process_excel_data(f_name):
     workbook = openpyxl.load_workbook(f_name)
     sheet = workbook.active
-    props = [str(cell.value).strip() for cell in sheet[1] if cell.value]
     persons_list = []
     for row_index, row in enumerate(sheet.iter_rows(), start=1):
-        if row_index == 1:
-            continue
-        if row_index > 11:
+        if row_index > NUMBER_OF_RECORDS + 1:
             break
         row_data = [str(cell.value).strip() for cell in row if cell.value]
+        if row_index == 1:
+            props = row_data
+            continue
         persons_list.append(Person(props, row_data))
     return persons_list
--- rpa_challenge_automation.py
+++ rpa_challenge_automation.py
@@ -4,30 +4,34 @@ import time
 from Modules import file_handling, web_forms, web_driver
 
 
-# Constants for URLs, folders, filenames, and button texts
+# Constants for URLs, folders, filenames, and button texts on the target web page
 WEBSITE_URL = "https://rpachallenge.com/"
 EXCEL_FILE_NAME = "challenge.xlsx"
-DATA_DIR = "data"
+DATA_DIR = "dat"
 DOWNLOAD = "Download Excel"
 START = "Start"
 SUBMIT = "Submit"
 
-# Create the "data" folder in the root directory and setup driver to download to this folder
+# Create the DATA_DIR folder in the root directory and setup driver to download EXCEL_FILE_NAME file into this folder
 data_dir = os.path.join(os.path.dirname(__file__), DATA_DIR)
-os.makedirs(data_dir, exist_ok=True)
+try:
+    os.makedirs(data_dir, exist_ok=True)
+except Exception as e:
+    print(f"Cannot create the folder for download: {e}")
+    sys.exit(1)
+
+f_name = f"./{DATA_DIR}/{EXCEL_FILE_NAME}"
 driver = web_driver.setup(DATA_DIR)
 
 # further manipulations with data and web forms
 try:
-    file_handling.download_excel(
-        driver, WEBSITE_URL, DOWNLOAD, EXCEL_FILE_NAME, DATA_DIR
-    )
+    file_handling.download_excel(driver, WEBSITE_URL, DOWNLOAD, f_name)
 except Exception as e:
     print(f"Error during download: {e}")
     sys.exit(1)
 
 try:
-    list_of_persons = file_handling.process_excel_data(EXCEL_FILE_NAME, DATA_DIR)
+    list_of_persons = file_handling.process_excel_data(f_name)
 except Exception as e:
     print(f"Error processing Excel data: {e}")
     sys.exit(1)
@@ -41,5 +45,5 @@ except Exception as e:
 else:
     print("All done! Finishing...")
 
-time.sleep(1)
+time.sleep(2)
 driver.quit()
