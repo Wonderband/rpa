af8634880b7360b6aefff3a92fe6f360beaa54ee
Alex Koniushenko <alex.koniushenko@gmail.com>
2023-08-28 11:21:37 +0300

    optimizing

--- Modules/web_forms.py
+++ Modules/web_forms.py
@@ -15,16 +15,12 @@ def start_challenge(driver, start_text):
 def fill_form_with_data(driver, persons_list, submit_value):
     for person in persons_list:
         for key, value in vars(person).items():
-            label_text = key
-            label_element = driver.find_element(
-                By.XPATH, f'//label[text()="{label_text}"]'
-            )
+            label_element = driver.find_element(By.XPATH, f'//label[text()="{key}"]')
             input_element = label_element.find_element(
                 By.XPATH, "./following-sibling::input"
             )
-            input_value = value
             input_element.clear()
-            input_element.send_keys(input_value)
+            input_element.send_keys(value)
         submit = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable(
                 (By.XPATH, f'//input[@type="submit" and @value="{submit_value}"]')
